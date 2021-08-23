f6e0003e497411454e9d38b180570e197074a006
FHChen0420 <1504403484@qq.com>
2021-08-23 09:24:46 +0800

    更新接口请求UA

--- API.py
+++ API.py
@@ -9,7 +9,7 @@ class BaseAPI:
     def __init__(self,timeout=5):
         self.timeout=timeout
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78",
         }
     
     def set_default_timeout(self,timeout=5):
