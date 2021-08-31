361f42a1b9c43d5c912dcff04c98928894cc3c73
FHChen0420 <1504403484@qq.com>
2021-08-31 15:52:34 +0800

    PlayerFrame根据win/mac系统选择相应内核

--- PlayerFrame.py
+++ PlayerFrame.py
@@ -3,10 +3,12 @@ import wx.html2
 
 class PlayerFrame(wx.Frame):
     def __init__(self, parent):
-        if not wx.html2.WebView.IsBackendAvailable(wx.html2.WebViewBackendEdge):
+        if parent.platform=="win" and not wx.html2.WebView.IsBackendAvailable(wx.html2.WebViewBackendEdge):
             return wx.MessageBox("＊请检查电脑是否安装了Edge/Chrome浏览器\n"+
             "＊请检查工具目录下是否存在对应版本(x64或x86)的WebView2Loader.dll文件","自带窗口打开失败")
         wx.Frame.__init__(self, parent, -1, "LivePlayer", size=(1280,720),style=wx.DEFAULT_FRAME_STYLE)
-        self.browser = wx.html2.WebView.New(self,backend=wx.html2.WebViewBackendEdge)
+        backend=wx.html2.WebViewBackendEdge if parent.platform=="win" else wx.html2.WebViewBackendWebKit
+        self.browser = wx.html2.WebView.New(self,backend=backend)
         self.browser.LoadURL("http://127.0.0.1:8080/player.html")
         self.Show()
+        
