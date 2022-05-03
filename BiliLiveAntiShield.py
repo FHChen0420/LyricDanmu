9c4ab0809ad4a45a478fc14615a5ddcb52a1af0e
FHChen0420 <1504403484@qq.com>
2022-05-03 15:49:58 +0800

    规范私有变量的命名

--- BiliLiveAntiShield.py
+++ BiliLiveAntiShield.py
@@ -10,21 +10,21 @@ class BiliLiveAntiShield:
         :param: rules 正则处理字典[正则匹配串:正则捕获处理函数/字符串]（用于处理较复杂规则）
         :param: words 屏蔽词列表（用于处理较简单规则）
         :param: filler 用于填充屏蔽词的单字符，默认为U+E0020'''
-        self._filler=filler
-        self._single_fill=lambda x: x.group()[0]+self._filler+x.group()[1:]
-        self._multi_fill=tuple([lambda x,i=i: x.group(1)+self._fill(x.group(2),i) for i in range(10)])
-        self._deal_list:List[Tuple[Pattern,Replace]]=[]
+        self.__filler=filler
+        self.__single_fill=lambda x: x.group()[0]+self.__filler+x.group()[1:]
+        self.__multi_fill=tuple([lambda x,i=i: x.group(1)+self.__fill(x.group(2),i) for i in range(10)])
+        self.__deal_list:List[Tuple[Pattern,Replace]]=[]
         for pat,rep in rules.items():
-            self._deal_list.append((re.compile(pat),rep))
+            self.__deal_list.append((re.compile(pat),rep))
         for word in words:
-            self._generate_rule(word)
+            self.__generate_rule(word)
 
-    def _fill(self,string:str,length:int) -> str:
+    def __fill(self,string:str,length:int) -> str:
         '''使用填充符来填补字符串string，使其中的非空格字符数等于length'''
-        dots=self._filler*(length-len(string)+string.count(" "))
+        dots=self.__filler*(length-len(string)+string.count(" "))
         return string+dots
 
-    def _generate_rule(self,word:str) -> None:
+    def __generate_rule(self,word:str) -> None:
         '''根据屏蔽词word，生成相应的匹配模式及替换规则，添加到列表deal_list中'''
         # word中，“#”后的数字表示至少需要间隔多少个非空格字符才不会被屏蔽。
         # 如果word不含“#”，则在第一个字符后添加一个填充符来进行处理。
@@ -33,7 +33,7 @@ class BiliLiveAntiShield:
             n=len(parts)-1
             if n==0:
                 pat = "(?i)"+" ?".join(word)
-                self._deal_list.append((re.compile(pat),self._single_fill))
+                self.__deal_list.append((re.compile(pat),self.__single_fill))
                 return
             distance=[int(i) for i in re.findall(r"#([1-9])",word)]
             regex1,regex2="",""
@@ -47,12 +47,12 @@ class BiliLiveAntiShield:
                 regex=" ?" if distance[i]==1 else "(?: ?\S){0,%d} ?"%(distance[i]-1)
                 regex2+=regex+parts[i+1]
             pat="(?i)(%s)(%s)(?=%s)"%(parts[0],regex1,parts[1]+regex2)
-            self._deal_list.append((re.compile(pat),self._multi_fill[distance[0]]))
+            self.__deal_list.append((re.compile(pat),self.__multi_fill[distance[0]]))
         except: pass
 
     def deal(self,string:str) -> str:
         '''对字符串string进行反屏蔽处理'''
-        for i in self._deal_list:
+        for i in self.__deal_list:
             try: 
                 string = i[0].sub(i[1],string)
             except:
